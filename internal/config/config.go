1306dee2962f1a966f743bdb81c921068ea2398b
Jonathan Giffard <39745127+LackOfMorals@users.noreply.github.com>
2025-09-04 11:46:28 +0200

    This introduces a change to config.go to support the use of .env file that holds the needed environmental variables and their values
    A new function, getEnvFilePath, has been written that returns the full folder path to where the mcp binary is located as this is where we expect the .env file to be.
    
    getEnvFilePath uses filepath.Abs(os.Args[0]) which to obtain full folder path and the binary filename which is then split with filepath.Split to obtain
    the full folder path which is then returned.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
 )
 
 // Config holds the application configuration
@@ -12,8 +16,36 @@ type Config struct {
 	Database string
 }
 
+func getEnvFilePath() string {
+	// The .env file has to be in the same folder as the neo4j-mcp binary
+	// Get the filepath to the neo4j-mcp binary
+	binaryFullPath, _ := filepath.Abs(os.Args[0])
+
+	// filepath.Abs returns full folder path and the binary filename but we only want the full folder path so we
+	// use filepath.Split that returns two values, dir = full folder path  and
+	// file = the binary name
+	// We just want the path which is where the .env file ( hopefully ) will be
+	envFolderPath, _ := filepath.Split(binaryFullPath)
+
+	return envFolderPath
+}
+
 // LoadConfig loads configuration from environment variables with defaults
 func LoadConfig() *Config {
+
+	// try loading .env and set the needed
+	// environmental variables
+	envFilePath := getEnvFilePath()
+	envFileName := ".env"
+
+	err := godotenv.Load(envFilePath + envFileName)
+
+	// We didn't load the .env file :(
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could  not find .env file in %s\n", envFilePath)
+		fmt.Fprintf(os.Stderr, "Trying Environment variables\n")
+	}
+
 	return &Config{
 		URI:      getEnv("NEO4J_URI", "bolt://localhost:7687"),
 		Username: getEnv("NEO4J_USERNAME", "neo4j"),
