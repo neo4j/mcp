07827ed8716a033efe00414b09049cc4fbc9af97
Michael Webb <michael.webb@neo4j.com>
2025-10-17 11:29:14 +0200

    test: add integration test helpers for easily adding new tests
    Tests will use testcontainers where we spin up a Neo4j database and run
    our integration tests against them.
    
    As there was a deliberate choice to use only one container (as opposed
    to a container for each test), there's a need to isolate tests, which we
    do by adding test IDs to data added to the database. This way each test
    is responsible for its own data creation and cleanup.
    
    Along with this there are various Neo4j data structure specific assertion
    helper functions added. This is quite opinionated, but the aim is to make
    it easy for contributors to add new tests.
    
    Note that waitForConnectibityNoT is a custom function which waits for
    Neo4j to be ready to accept connections using exponential backoff retry
    logic. It's used during test setup to ensure the database container is
    fully initialized before running tests.

---
+++ test/integration/integration_test.go
@@ -0,0 +1,54 @@
+package integration_test
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/neo4j/mcp/internal/config"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+var sharedCfg *config.Config
+var sharedDrv neo4j.DriverWithContext
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+
+	ctr, boltURI, err := createNeo4jContainer(ctx)
+	if err != nil {
+		log.Fatalf("failed to start shared neo4j container: %v", err)
+	}
+
+	username := os.Getenv("NEO4J_USERNAME")
+	if username == "" {
+		username = "neo4j"
+	}
+	password := os.Getenv("NEO4J_PASSWORD")
+	if password == "" {
+		password = "password"
+	}
+
+	drv, err := neo4j.NewDriverWithContext(boltURI, neo4j.BasicAuth(username, password, ""))
+	if err != nil {
+		_ = ctr.Terminate(ctx)
+		log.Fatalf("failed to create driver: %v", err)
+	}
+
+	if err := waitForConnectivityNoT(ctx, ctr, &drv); err != nil {
+		_ = drv.Close(ctx)
+		_ = ctr.Terminate(ctx)
+		log.Fatalf("failed to verify connectivity: %v", err)
+	}
+
+	sharedCfg = &config.Config{URI: boltURI, Username: username, Password: password, Database: "neo4j"}
+	sharedDrv = drv
+
+	code := m.Run()
+
+	_ = drv.Close(context.Background())
+	_ = ctr.Terminate(context.Background())
+
+	os.Exit(code)
+}
