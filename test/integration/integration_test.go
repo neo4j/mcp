f7c751d58807fab9bdadb9fd76d5ba75f4e737cc
Michael Webb <michael.webb@neo4j.com>
2025-10-22 14:27:45 +0200

    fix import and formatting

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -49,12 +49,12 @@ func TestMain(m *testing.M) {
 
 	code := m.Run()
 
-    if err := drv.Close(context.Background()); err != nil {
-      log.Printf("Warning: failed to close driver: %v", err)
-    }
-    if err := ctr.Terminate(context.Background()); err != nil {
-      log.Printf("Warning: failed to terminate container: %v", err)
-    }
+	if err := drv.Close(context.Background()); err != nil {
+		log.Printf("Warning: failed to close driver: %v", err)
+	}
+	if err := ctr.Terminate(context.Background()); err != nil {
+		log.Printf("Warning: failed to terminate container: %v", err)
+	}
 
 	os.Exit(code)
 }
