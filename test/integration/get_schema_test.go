afb58f6dda8c213f5705667900ac5339cbcfeb88
MacondoExpress <simone.gammicchia@gmail.com>
2025-10-31 13:13:49 +0000

    refactor get-schema test, moved specific assertion to the test file, removed empty labels returned by the get-schema to optimise token consumption

--- test/integration/get_schema_test.go
+++ test/integration/get_schema_test.go
@@ -3,14 +3,56 @@
 package integration
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/neo4j/mcp/internal/tools/cypher"
 	"github.com/neo4j/mcp/test/integration/helpers"
 )
 
+type SchemaPropertyType struct {
+	Array     bool   `json:"array"`
+	Existence bool   `json:"existence"`
+	Indexed   bool   `json:"indexed"`
+	Type      string `json:"type"`
+}
+
+type RelationshipType struct {
+	Count      int                           `json:"count"`
+	Direction  string                        `json:"direction"`
+	Labels     []string                      `json:"labels"`
+	Properties map[string]SchemaPropertyType `json:"properties"`
+}
+
+// SchemaValue represents the guaranteed structure inside the "value" field.
+type SchemaValue struct {
+	Properties    map[string]SchemaPropertyType `json:"properties"`
+	Relationships map[string]RelationshipType   `json:"relationships"`
+	Type          string                        `json:"type"`
+}
+
+// SchemaEntry represents one element of the returned by get-schema :
+// { "key": "...", "value": { ... } }
+type SchemaEntry struct {
+	Key   string      `json:"key"`
+	Value SchemaValue `json:"value"`
+}
+
 func TestGetSchema(t *testing.T) {
 	// do not run this test in Parallel, test-id will not avoid collision when testing the emptiness of the db.
+	t.Run("get-schema should give hint when the database is empty", func(t *testing.T) {
+		tc := helpers.NewTestContext(t, dbs.GetDriver())
+
+		getSchema := cypher.GetSchemaHandler(tc.Deps)
+		res := tc.CallTool(getSchema, nil)
+
+		textContent := tc.ParseTextResponse(res)
+
+		expectedMessage := "The get-schema tool executed successfully; however, since the Neo4j instance contains no data, no schema information was returned."
+		if textContent != expectedMessage {
+			t.Fatal("no empty schema hint returned")
+		}
+	})
 	t.Run("get-schema should collect information about the neo4j database", func(t *testing.T) {
 		tc := helpers.NewTestContext(t, dbs.GetDriver())
 
@@ -27,69 +69,96 @@ func TestGetSchema(t *testing.T) {
 		getSchema := cypher.GetSchemaHandler(tc.Deps)
 		res := tc.CallTool(getSchema, nil)
 
-		var schemaArray []map[string]any
-		tc.ParseJSONResponse(res, &schemaArray)
+		var schemaEntries []SchemaEntry
+		tc.ParseJSONResponse(res, &schemaEntries)
 
-		if len(schemaArray) == 0 {
+		if len(schemaEntries) == 0 {
 			t.Fatal("expected schema to contain at least one entry")
 		}
+		assertSchemaHasLabel(t, schemaEntries, personLabel.String())
+		assertSchemaHasLabel(t, schemaEntries, companyLabel.String())
 
-		schemaMap := make(map[string]map[string]any)
-		for _, entry := range schemaArray {
-			key, ok := entry["key"].(string)
-			if !ok {
-				continue
-			}
-			value, ok := entry["value"].(map[string]any)
-			if !ok {
-				continue
-			}
-			schemaMap[key] = value
+		personEntry := getSchemaEntryByTypeOrLabel(schemaEntries, personLabel.String())
+		personProperties := map[string]SchemaPropertyType{
+			"name": {
+				Indexed:   false,
+				Array:     false,
+				Existence: false,
+				Type:      "STRING",
+			},
+			"age": {
+				Indexed:   false,
+				Array:     false,
+				Existence: false,
+				Type:      "INTEGER",
+			},
 		}
+		assertSchemaEntryHasProperties(t, personEntry.Value.Properties, personProperties, true)
 
-		// Check for the unique labels created
-		assertSchemaHasNodeType(t, schemaMap, personLabel.String(), []string{"name", "age"})
-		assertSchemaHasNodeType(t, schemaMap, companyLabel.String(), []string{"name", "founded"})
+		companyEntry := getSchemaEntryByTypeOrLabel(schemaEntries, companyLabel.String())
+		companyProperties := map[string]SchemaPropertyType{
+			"name": {
+				Indexed:   false,
+				Array:     false,
+				Existence: false,
+				Type:      "STRING",
+			},
+			"founded": {
+				Indexed:   false,
+				Array:     false,
+				Existence: false,
+				Type:      "INTEGER",
+			},
+		}
+		assertSchemaEntryHasProperties(t, companyEntry.Value.Properties, companyProperties, true)
 	})
 
-	t.Run("get-schema should give hint when the database is empty", func(t *testing.T) {
-		tc := helpers.NewTestContext(t, dbs.GetDriver())
-
-		getSchema := cypher.GetSchemaHandler(tc.Deps)
-		res := tc.CallTool(getSchema, nil)
-
-		textContent := tc.ParseTextResponse(res)
-
-		expectedMessage := "The get-schema tool executed successfully; however, since the Neo4j instance contains no data, no schema information was returned."
-		if textContent != expectedMessage {
-			t.Fatal("no empty schema hint returned")
-		}
-	})
+	// TODO keep extending the coverage of the schema tests:
+	// - test different types such as float, duration etc ...
+	// - test primitive array such as []float
+	// - test Relationship
+	// - test indexes and constraints
 }
 
-// assertSchemaHasNodeType checks if the schema contains a node type with expected properties
-func assertSchemaHasNodeType(t *testing.T, schemaMap map[string]map[string]any, label string, expectedProps []string) {
-	t.Helper()
+// assertSchemaHasLabel checks if the schema contains a node type with expected label
+func assertSchemaHasLabel(t *testing.T, schemaEntries []SchemaEntry, label string) {
+	foundLabel := slices.ContainsFunc(schemaEntries, func(schemaEntry SchemaEntry) bool {
+		return schemaEntry.Key == label
+	})
 
-	schema, ok := schemaMap[string(label)]
-	if !ok {
-		t.Errorf("expected schema to contain '%s' label", label)
-		return
+	if !foundLabel {
+		t.Fatalf("label %s was not found in the schema", label)
 	}
+}
 
-	if schema["type"] != "node" {
-		t.Errorf("expected %s type to be 'node', got %v", label, schema["type"])
-	}
+func getSchemaEntryByTypeOrLabel(schemaEntries []SchemaEntry, labelOrType string) SchemaEntry {
+	idx := slices.IndexFunc(schemaEntries, func(schemaEntry SchemaEntry) bool {
+		return schemaEntry.Key == labelOrType
+	})
 
-	props, ok := schema["properties"].(map[string]any)
-	if !ok {
-		t.Errorf("expected %s to have properties", label)
-		return
-	}
+	return schemaEntries[idx]
+}
 
-	for _, prop := range expectedProps {
-		if _, exists := props[prop]; !exists {
-			t.Errorf("expected %s to have '%s' property", label, prop)
+func assertSchemaEntryHasProperties(t *testing.T, entryProperties map[string]SchemaPropertyType, expectedProperties map[string]SchemaPropertyType, validateMeta bool) {
+	for name, expected := range expectedProperties {
+		got, ok := entryProperties[name]
+		if !ok {
+			t.Fatalf("property %s expected for schema properties but not found, found properties: %v", name, entryProperties)
+		}
+
+		if validateMeta {
+			if got.Array != expected.Array {
+				t.Fatalf("property %s not found: Array mismatch (expected=%t got=%t)", name, expected.Array, got.Array)
+			}
+			if got.Existence != expected.Existence {
+				t.Fatalf("property %s not found: Existence mismatch (expected=%t got=%t)", name, expected.Existence, got.Existence)
+			}
+			if got.Indexed != expected.Indexed {
+				t.Fatalf("property %s not found: Indexed mismatch (expected=%t got=%t)", name, expected.Indexed, got.Indexed)
+			}
+			if got.Type != expected.Type {
+				t.Fatalf("property %s not found: Type mismatch (expected=%s got=%s)", name, expected.Type, got.Type)
+			}
 		}
 	}
 }
