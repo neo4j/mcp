3b7114e7dd515f44fa089de13aaf0849e895e423
MacondoExpress <simone.gammicchia@gmail.com>
2025-11-12 13:55:34 +0000

    apply demorgan's law on config checks

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,7 +21,7 @@ func (c *Config) Validate() error {
 		return fmt.Errorf("configuration is required but was nil")
 	}
 	// TODO: support different Config types with validation https://linear.app/neo4j/issue/DEVSURF-873/support-different-types-in-the-mcp-config
-	if !(c.Telemetry == "false" || c.Telemetry == "true") {
+	if c.Telemetry != "false" && c.Telemetry != "true" {
 		return fmt.Errorf("%s cannot be converted to type %s", "NEO4J_TELEMETRY", "bool")
 	}
 
