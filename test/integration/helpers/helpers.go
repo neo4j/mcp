7a515ac34d55e58e9de45c008bf42a3917a731ca
Kerem Gocen <kerem.gocen@neo4j.com>
2026-01-15 11:48:42 +0000

    remove the duplicate line

--- test/integration/helpers/helpers.go
+++ test/integration/helpers/helpers.go
@@ -87,7 +87,6 @@ func getAnalyticsMock(t *testing.T) *analytics.MockService {
 	analyticsService.EXPECT().Disable().AnyTimes()
 	analyticsService.EXPECT().Enable().AnyTimes()
 	analyticsService.EXPECT().NewGDSProjCreatedEvent().AnyTimes()
-	analyticsService.EXPECT().NewGDSProjCreatedEvent().AnyTimes()
 	analyticsService.EXPECT().NewStartupEvent().AnyTimes()
 	analyticsService.EXPECT().NewToolEvent(gomock.Any(), gomock.Any()).AnyTimes()
 	analyticsService.EXPECT().NewConnectionInitializedEvent(gomock.Any()).AnyTimes()
